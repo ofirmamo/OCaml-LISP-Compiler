09401b4acca6750b0fbca9b5488d33c4b3e90b6c
Ofir Mamo <ofirmamo@gmail.com>
2019-01-03 00:33:34 +0200

    special chars validation

---
+++ tests/special-chars/speical-chars.scm
@@ -0,0 +1,25 @@
+#\newline
+#\space
+#\nul
+#\return
+#\tab
+#\page
+
+#\a
+#\A
+#\?
+
+#\x30
+#\xa
+
+"moshe"
+"a string"
+
+" This is very
+long long string
+that spills acrros several
+lines."
+
+" \\ \" \t \f \n \r"
+
+"h \xa; "
